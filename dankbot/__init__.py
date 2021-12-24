a9a5c006276b572724519fc99576a53fdb9ca18f
NarayanAdithya <narayanadithya1234@gmail.com>
2021-12-24 21:00:19 +0530

    Added Class and Drivers:

---
+++ dankbot/__init__.py
@@ -0,0 +1,1 @@
+from dankbot.dankbot import DankBot
--- dankbot/dankbot.py
+++ dankbot/dankbot.py
@@ -1,4 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 
-def init():
-    print("Init Project Linting Check")
+class DankBot():
+    """
+    DankBot Class Initialises the Headless browser followed by the necessary automation fucntionalities.
+    Controls the curses inlay.
+    """
+    TYPE_BOX = 'markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP'
+    BASE_URL = 'https://discord.com'
+    DEF_BROWSER = "Chrome"
+
+    def __init__(self, def_brow='Chrome'):
+        self.DEF_BROWSER = def_brow
+        while(1):
+            if self.DEF_BROWSER == 'Chrome':
+                driver = webdriver.Chrome()
+            elif self.DEF_BROWSER == 'Edge':
+                driver = webdriver.Edge()
+            else:
+                print("To be Done")
+        driver.get(self.BASE_URL)
+        type_box = driver.find_element_by_class_name(self.TYPE_BOX)
+
+    def exec_base_script(self):
+        self.type_box.send_keys('pls beg')
+        self.type_box.send_keys(Keys.ENTER)
+        sleep(5)
+        self.type_box.send_keys('pls hunt')
+        self.type_box.send_keys(Keys.ENTER)
+        sleep(5)
+        self.type_box.send_keys('pls dig')
+        self.type_box.send_keys(Keys.ENTER)
+        sleep(5)
+        self.type_box.send_keys('pls fish')
+        self.type_box.send_keys(Keys.ENTER)
+        sleep(5)
+        self.type_box.send_keys('pls use horseshoe')
+        self.type_box.send_keys(Keys.ENTER)
+        sleep(5)    
